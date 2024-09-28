9da433fd149b32abcb880edc72848a5d41f7da3e
Goyal1 <puneet_goyal1@dell.com>
2024-09-28 12:17:01 +0530

    first commit

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React,  {useState} from "react";
+
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import { TooltipTrigger, TooltipContent, TooltipProvider, Tooltip } from "@/components/ui/tooltip";
+
+
+const projects = [
+    {
+        num: '01',
+        category: 'frontend',
+        title: 'ReactJS',
+        description: 'A JavaScript library for building user interfaces',
+        stacl : [{
+            name: 'React',
+            icon: 'react'
+        }, {
+            name: 'JavaScript',
+            icon: 'javascript'
+        }, {
+            name: 'HTML',
+            icon: 'html'
+        }, {
+            name: 'CSS',
+            icon: 'css'
+        }],
+        image:'/'
+    }
+]
+
 const Work = () => {
     return (
         <div>Work page</div>
